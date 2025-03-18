9bab93d8b1a7b332a02ba4c069344f6e3025af3e
Jiajia Chen <jiajchen@f0r2s9.codam.nl>
2025-03-18 17:00:17 +0100

    user control for chat room

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -12,25 +12,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room = None
 
     async def connect(self):
-        from .models import ChatRoom
-
         user = self.scope['user']
         if user.is_anonymous:
             await self.close(code=1006)
             return
         self.room_id = int(self.scope['url_route']['kwargs']['room_id'])
-        try:
-            self.room = await database_sync_to_async(ChatRoom.objects.get)(id=self.room_id)
-        except ObjectDoesNotExist:
+        is_valid_user, error_code = await self.is_user_in_room(self.scope['user'])
+        if not is_valid_user:
             await self.accept()
-            await self.close(code=4000)
-            return
-        if user != self.room.user1 and user != self.room.user2:
-            await self.accept()
-            await self.close(code=4001)
+            await self.close(code=error_code)
             return
         self.room_group_name = f'chat_{self.room_id}'
-        self.username = user.username
+        self.username = self.scope['user'].username
         self.redis = self.scope['redis_pool']
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -40,6 +33,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
         key_user_count = f'chat:{self.room_group_name}:user_count'
         await self.redis.incr(key_user_count)
         await self.load_previous_messages()
+    
+    @database_sync_to_async
+    def is_user_in_room(self, user):
+        from .models import ChatRoom
+
+        try:
+            self.room = ChatRoom.objects.get(id=self.room_id)
+            if user == self.room.user1 or user == self.room.user2:
+                return True, 0
+            return False, 4001
+        except ObjectDoesNotExist:
+            return False, 4000
 
     async def load_previous_messages(self):
         history_messages = await database_sync_to_async(lambda: self.room.history)()
