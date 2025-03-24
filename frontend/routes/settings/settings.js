5962bea62dfaae204eb6a4efc1967665bd845557
cdalla-s <cdalla-s@codam.student.nl>
2025-03-24 15:47:03 +0100

    added image upload

--- frontend/routes/settings/settings.js
+++ frontend/routes/settings/settings.js
@@ -8,7 +8,7 @@ export const init = () => {
     const username = document.getElementById('settings-username')
     const profileImg = document.getElementById('settings-img')
     const confirm = document.getElementById('confirm')
-    
+    const imgUpload = document.getElementById('image-upload')
     const closeBtn = document.getElementById('close-button')
     closeBtn.innerHTML = `<i class='bx bx-x-circle'></i>`
     
@@ -23,11 +23,57 @@ export const init = () => {
         location.hash = '/'
     }
     
+    function triggerChangeImg() {
+        imgUpload.click()
+    }
+
+    function changeProfileImg(event) {
+        const file = event.target.files[0];
+        if (file) {
+
+            //change avatar API
+            if (changeAvatar(file)) {
+                //change profile img
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    const imageUrl = e.target.result;
+                    profileImg.src = imageUrl;
+                };
+                // reader.readAsDataURL(file);
+            }
+        }
+    }
+
+    async function changeAvatar (file) {
+        const formData = new FormData()
+        formData.append('avatar', file)
+
+        const isTokenValid = await checkToken()
+        if (!isTokenValid) return
+        const response = await fetch(`http://${window.location.host}/api/users/avatar/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "multipart/form-data",
+                "Authorization": `Bearer ${getUserToken().access}`
+            },
+            body: formData,
+        });
+        
+        if (response.ok) {
+            return true
+        } else {
+            console.log("error message failed to upload avatar")
+            //return false
+        }
+    }
+
     function submitForm () {form.requestSubmit()}
     
     function deleteEvents () {
         confirm.removeEventListener('click', submitForm)
         closeBtn.removeEventListener('click', closeSettings)
+        imgUpload.removeEventListener('change', changeAvatar)
+        profileImg.removeEventListener('click', triggerChangeImg)
     }
     
     function clearInputs () {
@@ -73,13 +119,16 @@ export const init = () => {
             } else {
                 const errorData = await response.json()
                 console.log("error message", errorData.new_password)
+                //want to get message error from response
                 // const errorMessage = errorData.detail || "Password change failed. Please check your credentials."
-                // message.innerHTML = `<p class='text-danger'>${errorMessage}</p>`
+                message.innerHTML = `<p class='text-danger'>${errorMessage}</p>`
                 clearInputs()
             }
         }
     }
     
+    imgUpload.addEventListener('change', changeProfileImg)
+    profileImg.addEventListener('click', triggerChangeImg)
     confirm.addEventListener('click', submitForm)
     closeBtn.addEventListener('click', closeSettings)
 }
