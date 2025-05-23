f796377ae27d52fedc5c1e354b559ebccf5460aa
Rik Verhoeven <rikverhoeven10@gmail.com>
2025-05-23 08:41:12 +0200

    moved social menu init functions into if statement in onload.js; no more get request error from these functions with loading page. Register works again

--- frontend/social/buttons.js
+++ frontend/social/buttons.js
@@ -1,4 +1,17 @@
 
+// const socialmenubuttons_class = document.querySelector("social-menu-button")
+const show_requests_button = document.getElementById("show-requests")
+const send_requests_button = document.getElementById("send-requests")
+const chat_search_button = document.getElementById("chat")
+// addEventListener
+
+// socialmenubuttons_class.addEventListener('click', async (event) => {
+
+//     socialmenubuttons_class(socialmenubuttons_class, )
+
+// }
+// )
+
 export default function showHide(elementId, typeOfDisplay) {
     // location.reload();
     const element = document.getElementById(elementId);
