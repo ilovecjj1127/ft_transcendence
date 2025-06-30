90a26df7d2e01865a927933369dc3dc1cdd27a54
Your Name <you@example.com>
2025-06-30 20:37:10 +0200

    moved GeneralSocialDataConsumer in backend to temp folder, so deleted in git push

--- backend/chat/urls/ws_urls.py
+++ backend/chat/urls/ws_urls.py
@@ -7,7 +7,3 @@ from chat.views import room
 urlpatterns = [
     path('room/<int:room_id>/', room, name='room'),
 ]
-
-websocket_urlpatterns = [
-    re_path(r"ws/chat/general_social_data_socket_on_backend/?$", consumers.GeneralSocialConsumer.as_asgi()),
-]
