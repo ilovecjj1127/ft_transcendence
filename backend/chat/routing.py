a9657b5bf7914ac05a87a67353fc7f587760441a
Your Name <you@example.com>
2025-06-30 20:52:44 +0200

    comment out initial message in html, about 'message' field, also moved routing.py

--- backend/chat/routing.py
+++ backend/chat/routing.py
@@ -7,5 +7,4 @@ from . import consumers
 
 chat_websocket_urlpatterns = [
     path('ws/chat/<int:room_id>/', ChatConsumer.as_asgi()),
-    re_path(r"ws/chat/general_social_data_socket_on_backend/?$", consumers.GeneralSocialConsumer.as_asgi()),
 ]
