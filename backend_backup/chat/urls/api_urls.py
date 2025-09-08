607eead125cab02c1badeb397209a08845de530b
Alexander Petukhov <alpetukh@student.codam.nl>
2025-09-08 19:50:34 +0200

    Deleted backend_backup folder

--- backend_backup/chat/urls/api_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from chat.views import room, ChatGetOrCreateView, ChatBlockorUnblockView
-
-
-urlpatterns = [
-    path('get_or_create/', ChatGetOrCreateView.as_view(), name='get_or_create_chatroom'),
-    path('block_or_unblock/', ChatBlockorUnblockView.as_view(), name='block_or_unblock_action'),
-]
--- backend_backup/games/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Game, Tournament, TournamentPlayer
-
-admin.site.register(Game)
-admin.site.register(Tournament)
-admin.site.register(TournamentPlayer)
-
-# Register your models here.
